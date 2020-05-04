b5565b331867a6b9cb493b075a00ad22cf6ae94c
PaiSoeMoe <paisoe1991@gmail.com>
2020-05-04 18:56:53 -0400

    rebuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from "ngx-socket-io";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  accounts = [];
   title = 'chat-front';
+  id;
+  friendsOnline;
+  txt = "";
+  to;
+  name = "";
+  joined = false;
+  constructor(private socket: Socket) {
+  }
+
+  ngOnInit() {
+
+    this.socket.on('socketID', (m) => {
+      this.id = m;
+    })
+
+    this.socket.on('list', (ms) => {
+
+      if (ms.length > 0) {
+        let ii = ms.find(u => {
+          return u.name === this.id.id;
+        });
+        let users = ms.filter(f => { return f !== ii })
+        this.friendsOnline = users.map(f => {
+          return { id: f, messages: [], newMs: false }
+
+        })
+        console.log(this.friendsOnline);
+      }
+    })
+
+    this.socket.on("message", (m) => {
+      console.log("message", m);
+      let account = this.friendsOnline.find(f => f.id.id === m.id);
+      console.log(account);
+      account.messages = account.messages.concat({ id: account.id, timestamp: Date.now(), message: m.message });
+      account.newMs = true;
+    })
+
+  }
+
+  join() {
+    this.socket.emit('register', this.name);
+    this.joined = true;
+    console.log(this.joined);
+  }
+
+  send() {
+    let account = this.friendsOnline.find(f => f.id === this.to.id);
+    console.log("i am id ", this.id);
+    account.messages = account.messages.concat({ id: { name: this.id.id }, timestamp: Date.now(), message: this.txt })
+    this.socket.emit("message", { to: this.to.id, message: this.txt })
+  }
+
+  select(f) {
+    this.to = f;
+  }
+
+  check(f) {
+    f.newMs = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+import { FormsModule } from "@angular/forms"
+
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+
 
 @NgModule({
   declarations: [
@@ -10,9 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+
+}
