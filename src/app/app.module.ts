5fb1514e72ffd2bf0f25d11be5a1f2283abbcc09
PaiSoeMoe <paisoe1991@gmail.com>
2020-05-07 18:42:46 -0400

    linked to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
 import { FormsModule } from "@angular/forms"
 
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'https://afternoon-thicket-23516.herokuapp.com/', options: {} };
 
 
 @NgModule({
